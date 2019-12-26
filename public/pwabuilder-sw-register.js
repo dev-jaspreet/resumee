8b5d407115422df77649d955ce243a1f4196d8cc
Ubuntu <ubuntu@ip-172-31-26-92.us-east-2.compute.internal>
2019-12-26 12:50:37 +0000

    hmm

--- public/pwabuilder-sw-register.js
+++ public/pwabuilder-sw-register.js
@@ -6,13 +6,14 @@
 if ("serviceWorker" in navigator) {
   if (navigator.serviceWorker.controller) {
     console.log("[PWA Builder] active service worker found, no need to register");
-  } else {
+  }
+  else {
     // Register the service worker
     navigator.serviceWorker
       .register("pwabuilder-sw.js", {
         scope: "./"
       })
-      .then(function (reg) {
+      .then(function(reg) {
         console.log("[PWA Builder] Service worker has been registered for scope: " + reg.scope);
       });
   }
