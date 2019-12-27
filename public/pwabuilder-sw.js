13ea4ae67f77ffa49e28767922462d69f8824b51
Ubuntu <ubuntu@ip-172-31-26-92.us-east-2.compute.internal>
2019-12-27 09:23:11 +0000

    hmm

--- public/pwabuilder-sw.js
+++ public/pwabuilder-sw.js
@@ -1,11 +1,11 @@
-// This is the "Offline copy of pages" service worker
+// This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
 
-const CACHE = "pwabuilder-offline";
+const CACHE = "pwabuilder-offline-page";
 
-// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "index.html";
+// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
 const offlineFallbackPage = "ToDo-replace-this-name.html";
 
-// Install stage sets up the index page (home page) in the cache and opens a new cache
+// Install stage sets up the offline page in the cache and opens a new cache
 self.addEventListener("install", function(event) {
   console.log("[PWA Builder] Install Event processing");
 
@@ -46,11 +46,16 @@ self.addEventListener("fetch", function(event) {
 function fromCache(request) {
   // Check to see if you have it in the cache
   // Return response
-  // If not in the cache, then return error page
+  // If not in the cache, then return the offline page
   return caches.open(CACHE).then(function(cache) {
     return cache.match(request).then(function(matching) {
       if (!matching || matching.status === 404) {
-        return Promise.reject("no-match");
+        // The following validates that the request was for a navigation to a new document
+        if (request.destination !== "document" || request.mode !== "navigate") {
+          return Promise.reject("no-match");
+        }
+
+        return cache.match(offlineFallbackPage);
       }
 
       return matching;
