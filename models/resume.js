eb6e195225883283da822e648ff176404d6fd7d7
dev-jaspreet <59387137+dev-jaspreet@users.noreply.github.com>
2019-12-31 13:11:37 +0300

    upload files

---
+++ models/resume.js
@@ -0,0 +1,22 @@
+var mongoose = require("mongoose"),
+    passportlocalmongoose = require("passport-local-mongoose");
+
+var resumeSchema = mongoose.Schema({
+    resumename: String,
+    availability: String,
+    project: Object,
+    education: Object,
+    certification: Object,
+    skill: Object,
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+    created: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+resumeSchema.plugin(passportlocalmongoose);
+module.exports = mongoose.model("Resume", resumeSchema);
